a6ed8030987b2216ed923899ff20187c86ae9dd3
Henrique Brandão <32486795+hb06ars@users.noreply.github.com>
2021-03-03 17:41:13 -0300

    .
    .

---
+++ src/main/java/brandaoti/sistema/ligaweb/LigawebApplication.java
@@ -0,0 +1,12 @@
+package brandaoti.sistema.ligaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LigawebApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LigawebApplication.class, args);
+	}
+
+}
